c4c57f0bceab2cabeed5e876ef147c1e086d6d97
Matheus Dantas <matheusdantas@ad.corp.google.com>
2020-11-14 16:55:11 -0300

    added team pictures to the events

--- src/app/pages/new-games/new-games.page.ts
+++ src/app/pages/new-games/new-games.page.ts
@@ -19,7 +19,6 @@ export class NewGamesPage implements OnInit {
     this.api.get().subscribe((data) =>{
       console.log(data);
       this.getEvents();
-      this.getTeste();
     });
 
 
@@ -28,16 +27,36 @@ export class NewGamesPage implements OnInit {
   ngOnInit() {
   }
 
-  getEvents(){
-    this.api.get('sportspress/v2/events').subscribe((data) =>{
-      this.events = data;
-    })
-  }
+  // getEvents(){
+  //   this.api.get('sportspress/v2/events').subscribe((data) =>{
+  //     this.events = data;
+  //   })
+  // }
 
-  getTeste(){
+  getEvents(){
     this.api.get('sportspress/v2/events').subscribe((events) =>{
       let x = events;
 
+      x.forEach(game =>{
+
+        this.api.get(`sportspress/v2/teams/${game.teams[0]}`).subscribe((team) => {
+          console.log(team.featured_media);
+  
+          this.api.get(`wp/v2/media/${team.featured_media}`).subscribe((teamPhoto) =>{
+          game.teamHome = teamPhoto.media_details.sizes.full.source_url;
+          
+          })
+        })  
+        this.api.get(`sportspress/v2/teams/${game.teams[1]}`).subscribe((team) => {
+          console.log(team.featured_media);
+  
+          this.api.get(`wp/v2/media/${team.featured_media}`).subscribe((teamPhoto) =>{
+          game.teamVisitor = teamPhoto.media_details.sizes.full.source_url;
+          
+        })
+      })
+      this.events = x;
+      })
       console.log(x)
     })
   }
